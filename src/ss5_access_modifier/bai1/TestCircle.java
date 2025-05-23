2ff816984df88e2adb08d0d5a51577fb58c825d0
hongochieu <hongochieu2611@gmail.com>
2025-05-23 20:47:09 +0700

    up

---
+++ src/ss5_access_modifier/bai1/TestCircle.java
@@ -0,0 +1,8 @@
+package ss5_access_modifier.bai1;
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Radius: " + circle.getRadius());
+        System.out.println("Area: " + circle.getArea());
+    }
+}
