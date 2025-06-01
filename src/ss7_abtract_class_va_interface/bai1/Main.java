ee547a982628359265bdca847d63f46218079ee1
hongochieu <hongochieu2611@gmail.com>
2025-06-01 19:49:11 +0700

    iterface

--- src/ss7_abtract_class_va_interface/bai1/Main.java
+++ src/ss7_abtract_class_va_interface/bai1/Main.java
@@ -1,7 +1,7 @@
 package ss7_abtract_class_va_interface.bai1;
 import java.util.Random;
 public class Main {
-    public static void Main(String[]args) {
+    public static void main(String[]args) {
         Resizeable[] shapes = new Resizeable[3];
         shapes[0] = new Circle(10);
         shapes[1] = new Rectangle(5, 10);
@@ -15,8 +15,22 @@ public class Main {
             double newArea = 0;
 
             if (shape instanceof Circle) {
-                Circle circle = new Circle()
+                Circle circle = (Circle) shape;
+                oldArea = circle.getArea();
+                circle.resize(percent);
+                newArea = circle.getArea();
+            } else if (shape instanceof Square) {
+                Square square = (Square) shape;
+                oldArea = square.getArea();
+                square.resize(percent);
+                newArea = square.getArea();
+            } else if (shape instanceof Rectangle) {
+                Rectangle rectangle = (Rectangle) shape;
+                oldArea = rectangle.getArea();
+                rectangle.resize(percent);
+                newArea = rectangle.getArea();
             }
+            System.out.printf("Before resize: %.2f, after resize with %.2f%%: %.2f\n", oldArea, percent, newArea);
         }
     }
 }
